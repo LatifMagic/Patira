f664d997d921c49a6d2fb97fbc35e4e5c5337d71
Abdelatif Bensahnoun <latifben26@gmail.com>
2024-04-13 16:10:45 +0100

    Blog Header

---
+++ components/MainNav.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { siteConfig } from "@/config/site";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+const MainNav = () => {
+  const pathname = usePathname();
+  return (
+    <nav className="flex items-center space-x-4 lg:space-x-6">
+      <Link href="/" className="mr-6 flex items-center space-x-2">
+        <Image
+          src="/icons/logo_big-removebg-preview.png"
+          alt="Logo"
+          width={34}
+          height={34}
+          className="mr-[-8px] "
+        />
+        <span className="font-bold text-xl text-foreground">
+          {siteConfig.name}
+        </span>
+      </Link>
+      <Link
+        href="/blog"
+        className={cn(
+          "text-sm font-medium transition-colors hover:text-primary hidden sm:inline-block",
+          pathname === "/blog" ? "text-foreground" : "text-foreground/60"
+        )}
+      >
+        Blog
+      </Link>
+      <Link
+        href="/about"
+        className={cn(
+          "text-sm font-medium transition-colors hover:text-primary hidden sm:inline-block",
+          pathname === "/about" ? "text-foreground" : "text-foreground/60"
+        )}
+      >
+        About
+      </Link>
+    </nav>
+  );
+};
+
+export default MainNav;
---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "Patira",
+  url: "https://patira.com",
+  description:
+    "Blog powered by Next.js 14, utilizing Shadcn, Velite and Tailwind",
+  author: "Abdelatif Ben",
+  links: {
+    github: "https://github.com/LatifMagic",
+    personalSite: "",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
